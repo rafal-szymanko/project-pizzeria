f77eb1fcc1ad189cb41309149ffc825842004a32
Rafal Szymanko <szymanko@gmail.com>
2020-04-08 12:20:25 +0200

    Add handler for booking & order button

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,48 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function() {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.slice(2);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+        const id = link.getAttribute('href').slice(1);
+        thisApp.activatePage(id);
+        window.location.hash = `#/${id}`;
+      });
+    }
+  },
+
+  activatePage(pageID) {
+    const thisApp = this;
+    for (let page of thisApp.pages){
+      page.classList.toggle(
+        classNames.pages.active, 
+        page.id == pageID);
+    }
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.pages.active, 
+        link.getAttribute('href').slice(1) == pageID);
+    } 
+  },
   initMenu: function() {
     const thisApp = this;
     for(let productData in thisApp.data.products) {
@@ -35,6 +75,7 @@ const app = {
   },
   init: function(){
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
